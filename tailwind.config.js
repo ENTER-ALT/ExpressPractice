16a028f0bd6d481c12e850516b47bbf9f534fe7b
ENTER-ALT <enter.alt.591@gmail.com>
2024-10-09 08:44:39 +0000

    done header.ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,20 @@ module.exports = {
   content: ["./views/**/*.ejs", "./views/*.ejs"],
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
       colors: {
         'primary': '#776E87',
         'secondary': '#4D4861',
-        'success': '#4D4861',
+        'success': '#DE9151',
         'danger': '#E00049',
         'safe': '#5CDE51',
         'alter': '#C251DE'
       },
+      boxShadow: {
+        'button': '1px 2px 4px rgba(0, 0, 0, 0.8)',
+      }
     },
   },
   plugins: [
